7115f7cbfd86491f230503ea4a43d64382265867
bigbuckalex <bigbuckalex@gmail.com>
2019-04-08 18:36:34 -0400

    created and populated mySQL database

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Hamburger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheeseburger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Burger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Cheeseburger", FALSE);
